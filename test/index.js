e77cb3b5c277f39131c6de5d7519ce7080848649
Richard-tate <62254930+Richard-tate@users.noreply.github.com>
2020-09-02 10:55:16 +0100

    more added

--- test/index.js
+++ test/index.js
@@ -1,7 +1,6 @@
 const container = document.querySelector(".container");
 const Header = document.createElement('header');
 const Nav_btn = document.createElement('button');
-const right_side = document.createElement('section');
 
 
 Header.classList.add('Header');
@@ -37,15 +36,17 @@ function openNav() {
     }else{
         x.style.display = 'block';
     }
+   
 }
 
 function makeList() {
     // Establish the array which acts as a data source for the list
     let listData = [
         '<a href="#">Home</a>',
-        '<a href="#">Products</a>',
         '<a href="#">About</a>',
-        '<a href="#">Contact</a>'  
+        '<a href="#">Contact</a>',
+        '<a href="#">Products</a>'
+          
     ],
 
     // Make the list
@@ -54,7 +55,6 @@ function makeList() {
     numberOfListItems = listData.length,
     listItem,
     i;
-
     // Add it to the page
     document.querySelector('.navbar').appendChild(listElement);
 
@@ -72,3 +72,39 @@ function makeList() {
 
 // Usage
 makeList();
+
+
+function makeCard(){
+    cardContainer = document.createElement('div');
+    cardContainer.classList.add('card_container');
+    document.querySelector('.container').appendChild(cardContainer);
+    for (i=0; i < 6; ++i){
+    card = document.createElement('div');
+    card.classList.add('card'+ i);
+    card.style.width = '200px';
+    card.style.height = '350px';
+    card.style.margin = '1em';
+
+    document.querySelector('.card_container').appendChild(card); 
+    
+    cardTitle = document.createElement('h2');
+    cardTitle.classList.add('Title'+ i);
+    cardTitle.innerHTML= 'Card Title'+ i;
+
+    card.appendChild(cardTitle);
+
+    cardText = document.createElement('p');
+    cardText.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis quod provident nulla recusandae delectus necessitatibus fugit distinctio porro odio quaerat ut architecto dolore dignissimos officia, sint sed vel est aut!';
+    cardText.classList.add('card_text');
+
+    card.appendChild(cardText);
+
+    cardImage =document.createElement('img');
+    cardImage.classList.add('CI')
+    cardImage.src = '/image/cursed.jpg';
+    card.appendChild(cardImage);
+
+    }
+}
+makeCard();
+
